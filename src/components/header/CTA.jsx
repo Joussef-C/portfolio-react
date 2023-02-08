ad0d7556c744043b6e69e6775677fd01f0aeb8c7
Joussef Calderon <darkman868@hotmail.com>
2023-02-08 20:00:42 +0000

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import { FaDownload } from 'react-icons/fa';
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="https://drive.google.com/file/d/1G6BU6kdZG1bbjVzdqA9yETfYwdeXTl0_/view?usp=sharing" target="_blank" rel="noreferrer" className="btn">
+      <a href="https://drive.google.com/file/d/1Sm62TaaBZrJzrwJRzUAE1QkWSY1p_Gdv/view?usp=sharing" target="_blank" rel="noreferrer" className="btn">
         Download CV <FaDownload/>
       </a>
     </div>
